85bffa8dc22a9bafa54d735bbb212ff975e2b0af
Dav939 <stonewach939@gmail.com>
2025-04-04 18:41:03 -0600

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyBYx3KpjyNQ1MSNtPpt2rF3WLfgbR5Cv3w",
-    authDomain: "angular-crud-12c12.firebaseapp.com",
-    projectId: "angular-crud-12c12",
-    storageBucket: "angular-crud-12c12.firebasestorage.app",
-    messagingSenderId: "816490311434",
-    appId: "1:816490311434:web:6044c7f4a0519a66c594f3"
+  production: true,
+    firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  googleCloud: {
+    apiKey: "",
   }
 };
 
